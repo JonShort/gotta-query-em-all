b5258b3306e397629ac715c571cededbc940a44c
JonShort <JonShort@users.noreply.github.com>
2020-06-11 16:43:50 +0100

    :bulb: initial schema documentation

--- index.js
+++ index.js
@@ -3,25 +3,48 @@ const { ApolloServer, gql } = require('apollo-server');
 const data = require('./data/pokemon.json');
 
 const typeDefs = gql`
+  """
+  Height of the pokemon, ranges between a minimum and maximum value
+  """
   type Height {
     minimum: String
     maximum: String
   }
 
+  """
+  Weight of the pokemon, ranges between a minimum and maximum value
+  """
   type Weight {
     minimum: String
     maximum: String
   }
 
+  """
+  Details of the evolution, ID can be used for further queries
+  """
   type Evolution {
     id: ID!
+    imgSrc: String
     name: String
   }
 
-  type Monster {
+  type Pokemon {
+    """
+    Definiting trait of the Pokemon e.g. "Seed Pokémon"
+    """
     classification: String
+    """
+       All eventual evolutions of the pokemon, should be ordered by sequence
+
+    e.g. [B, C] - A > B > C
+    """
     evolutions: [Evolution]
     height: Height
+    """
+       ID as in Pokedex
+
+    e.g. "050"
+    """
     id: ID!
     imgSrc: String
     name: String
@@ -32,10 +55,25 @@ const typeDefs = gql`
   }
 
   type Query {
-    pokemon: [Monster]!
-    pokemonById(id: String!): [Monster]!
-    pokemonByName(name: String!): [Monster]!
-    pokemonByType(type: String!): [Monster]!
+    pokemon: [Pokemon]!
+    """
+       Provide full ID as in Pokedex including leading zeros
+
+    e.g. id: "003"
+    """
+    pokemonById(id: String!): [Pokemon]!
+    """
+       Provide full or partial name, will return partial matches (not case sensitive)
+
+    e.g. name: "pika"
+    """
+    pokemonByName(name: String!): [Pokemon]!
+    """
+       Provide type (not case sensitive)
+
+    e.g. type: "poison"
+    """
+    pokemonByType(type: String!): [Pokemon]!
   }
 `;
 
@@ -53,7 +91,7 @@ const resolvers = {
     pokemonByType: (_, params) => {
       return data.filter((monster) => {
         return monster.types.some((type) => {
-          return type.toLowerCase().includes(params.type.toLowerCase());
+          return type.toLowerCase() === params.type.toLowerCase();
         });
       });
     },
